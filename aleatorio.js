90cdf26114e25d88bce7eeb3743c3680824c2eb1
vitoriafelih <117662997+vitoriafelih@users.noreply.github.com>
2024-08-29 16:43:23 -0300

    Create aleatorio.js

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Você..."];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)

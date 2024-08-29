429a3ebbdb496e2ba28ffb3b8f0eaa332d5df0b2
vitoriafelih <117662997+vitoriafelih@users.noreply.github.com>
2024-08-29 16:51:49 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o seu tipo de pele?",
+        alternativas: [
+            {
+                texto: "A) Oleosa",
+                afirmacao: [
+                    "Para você, a beleza está na naturalidade. Você opta por cuidados suaves, preferindo o mínimo de produtos e priorizando saúde."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Mista",
+                afirmacao: [
+                    "Você se expressa através da beleza, gosta de explorar diferentes looks e estilos, e nunca tem medo de ousar."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu principal objetivo com cuidados de pele?",
+        alternativas: [
+            {
+                texto: "A) Combater a oleosidade",
+                afirmacao: [
+                    " Você preza pela praticidade e prefere manter uma rotina de beleza simples, mas eficaz. Menos é mais no seu estilo.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Manter a hidratação",
+                afirmacao: [
+                    "Você se expressa através da beleza, gosta de explorar diferentes looks e estilos, e nunca tem medo de ousar."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quantos passos costuma ter sua rotina de cuidados diários com a pele?",
+        alternativas: [
+            {
+                texto: "A) 1-2 passos",
+                afirmacao: [
+                    "Você adora cuidar da pele e do cabelo com dedicação, sempre buscando produtos de qualidade para manter um visual impecável."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) 3-4 passos",
+                afirmacao: [
+                    "Você se expressa através da beleza, gosta de explorar diferentes looks e estilos, e nunca tem medo de ousar."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu produto de beleza indispensável?",
+        alternativas: [
+            {
+                texto: "A) Protetor solar",
+                afirmacao: [
+                    " Você preza pela praticidade e prefere manter uma rotina de beleza simples, mas eficaz. Menos é mais no seu estilo."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) Hidratante facial",
+                afirmacao: [
+                    "Você adora cuidar da pele e do cabelo com dedicação, sempre buscando produtos de qualidade para manter um visual impecável.."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "O que é mais importante em uma rotina de beleza?",
+        alternativas: [
+            {
+                texto: "A) Praticidade",
+                afirmacao: [
+                    " Você preza pela praticidade e prefere manter uma rotina de beleza simples, mas eficaz. Menos é mais no seu estilo."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Produtos de qualidade",
+                afirmacao: [
+                    "Para você, a beleza está na naturalidade. Você opta por cuidados suaves, preferindo o mínimo de produtos e priorizando saúde."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: " Qual é o seu tipo de perfume favorito?",
+        alternativas: [
+            {
+                texto: "A) Cítrico e refrescante",
+                afirmacao: [
+                    ""
+                ],
+            },
+            {
+                texto: "B) Floral e suave",
+                afirmacao: [
+                    ""
+                ],
+            },
+        ]
+    },
+];
